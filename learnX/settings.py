d6e1709f030aced8ab029501f76e40b3621fb6c5
Amena Akter Keya <amenaakterkeya31@gmail.com>
2024-10-02 18:38:31 +0600

    update_reauirements

--- learnX/settings.py
+++ learnX/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['"127.0.0.1", ".vercel.app"']
+CSRF_TRUSTED_ORIGINS = ['"http://127.0.0.1", ".vercel.app"']
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [

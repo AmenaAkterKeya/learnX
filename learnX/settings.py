aee2e08935c3975c7763636824ca32c21c01f78e
Amena Akter Keya <amenaakterkeya31@gmail.com>
2024-11-11 20:55:49 +0600

    cors_trusted_origins

--- learnX/settings.py
+++ learnX/settings.py
@@ -31,7 +31,7 @@ CSRF_TRUSTED_ORIGINS = ['"http://127.0.0.1", ".vercel.app"']
 # CORS settings
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5501",
+    "http://127.0.0.1:5500",
     "https://learn-x-seven.vercel.app",
     "https://amenaakterkeya.github.io"  
 ]

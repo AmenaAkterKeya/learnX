74563c495b2f064a3272b3dbfcd4ef8462cb0e96
Amena Akter Keya <amenaakterkeya31@gmail.com>
2024-11-11 20:40:44 +0600

    cors_trusted_origins

--- learnX/settings.py
+++ learnX/settings.py
@@ -30,12 +30,13 @@ CSRF_TRUSTED_ORIGINS = ['"http://127.0.0.1", ".vercel.app"']
 
 # CORS settings
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5501",
+    "https://learn-x-seven.vercel.app"
+]
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://127.0.0.1:5501',
-#     'https://learn-x-seven.vercel.app',
-    
-# ]
+
+
 CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
 
@@ -52,10 +53,11 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_filters',
     # 'django.contrib.sites',
+    'corsheaders',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    "corsheaders",
+   
     'accounts',
     'course',
 ]
